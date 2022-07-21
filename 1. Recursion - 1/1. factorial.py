5637d2aafaa838a46c792cb729b1355f8b11caa6
Aatish Langhee <49237031+AatishLanghee@users.noreply.github.com>
2022-07-21 16:14:40 +0530

    Update 1. factorial.py

--- 1. Recursion - 1/1. factorial.py
+++ 1. Recursion - 1/1. factorial.py
@@ -1,1 +1,20 @@
+# Question: Factorial
 
+# Calculate the factorial of given number using recursion.
+# By default, python has 1000 maximum recursion depth.
+# We can change the depth if we want to.
+
+def factorial(num: int) -> int:
+    # Base Case
+    if num == 0:
+        return 1
+
+    # Smaller Output (Induction Hypothesis or Assumption)
+    small_output: int = factorial(num - 1)
+
+    return num * small_output
+
+
+if __name__ == "__main__":
+    number: int = int(input("Enter the number :"))
+    print(factorial(number))
